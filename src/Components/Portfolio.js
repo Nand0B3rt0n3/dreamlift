fb2e918488b3ebc6aa1ba314198a19ceed9c83af
Fernando Noguera <90071351+Nand0B3rt0n3@users.noreply.github.com>
2022-05-09 19:11:42 +0200

    changes

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,7 +30,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>ANOTHER SERVICES</h1>
+            <h1>PORTFOLIO</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -27,7 +27,7 @@ class Resume extends Component {
 
       <div className="row education">
          <div className="three columns header-col">
-            <h1><span>TRIPS</span></h1>
+            <h1><span>CERTIFICATES</span></h1>
          </div>
 
          <div className="nine columns main-col">
@@ -43,7 +43,7 @@ class Resume extends Component {
       <div className="row work">
 
          <div className="three columns header-col">
-            <h1><span>OTHER SERVICES</span></h1>
+            <h1><span>EXPERIENCE</span></h1>
          </div>
 
          <div className="nine columns main-col">
@@ -56,7 +56,7 @@ class Resume extends Component {
       <div className="row skill">
 
          <div className="three columns header-col">
-            <h1><span>MOST REQUESTED DESTINATIONS</span></h1>
+            <h1><span>IT DETAILS</span></h1>
          </div>
 
          <div className="nine columns main-col">
